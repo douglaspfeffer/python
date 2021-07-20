c00e05fc5da999ded4e00eba557847828f8dfabd
Douglas <87678082+douglaspfeffer@users.noreply.github.com>
2021-07-19 23:38:37 -0300

    Subindo Exercícios

--- ex023.py
+++ ex023.py
@@ -5,28 +5,18 @@
 # centena=8
 # milhar=1
 
-'''numero=int(input('Digite um número inteiro entre 0 e 9999: '))
+numero=int(input('Digite um número inteiro entre 0 e 9999: '))
 unidade=numero%10
-#123 = 3
 dezena=((numero-unidade)/10)%10
-#((123-3)/10)%10 = ((120)/10)%10 = (12%10)=2
-centena=(((numero-unidade)/10)-dezena)/10
-#(((123-3)/10)-2)/10 = (((120)/10)-2)/10 = ((12-2)/10) = (10/10) = 1
-#milhar=centena%10
-print('Unidade: {}'.format(unidade))
-print('Dezena: {}'.format(dezena))
-print('Centena: {}'.format(centena))
-#print('Centena: {}'.format(milhar))
+centena=((((numero-unidade)/10)-dezena)/10)%10
+milhar=((((numero-unidade)/10)-dezena)/10)//10
+print('Unidade: {:.0f}\nDezena: {:.0f}\nCentena: {:.0f}\nMilhar: {:.0f}'.format(unidade,dezena,centena,milhar))
 
-print('Unidade: {}')
-print('Dezena: {}')
-print('Centena: {}')
-print('Milhar: {}')'''
-
+'''código de júlia
 valor = int(input("Digite um número inteiro entre 0 e 9999: "))
 milhar = int(valor / 1000)
 centena = int((valor%1000)/100)
 dezena = int(((valor%1000)%100)/10)
 unidade = int((((valor%1000)%100)%10))
-print("milhar: {} {} {} {}".format(milhar, centena, dezena, unidade))
+print("milhar: {} centena {} dezena {} unidade {}".format(milhar, centena, dezena, unidade))'''
 
