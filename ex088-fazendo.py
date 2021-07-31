0dc547769e83251b635c33a3c92d68d22e43259f
Douglas <87678082+douglaspfeffer@users.noreply.github.com>
2021-07-31 15:20:32 -0300

    upah lele -- jujuh que commitou

--- ex088-fazendo.py
+++ ex088-fazendo.py
@@ -2,19 +2,19 @@
 quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo
 em uma lista composta. """
 
-from random import randint
+#from random import randint
 
-lista=[0,0,0,0,0,0]
+lista=[]
 
 jogosGerados=int(input('Quantos jogos serão gerados? '))
 
-for linha in range(0,6):
+for linha in range(0,jogosGerados):
     for coluna in range(0,6):
         lista[linha][coluna]=int(input(f'Digite um valor para a linha {linha+1} coluna {coluna+1}: '))
 
-for linha in range(0,6):
+for linha in range(0,jogosGerados):
     print()
     for coluna in range(0,6):
-        print(f'[{lista[linha][coluna]:^5}]',end=' ')
+        print(f'{lista[linha][coluna]:^5}',end=' ')
 
 print('\n')
