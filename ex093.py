b800c58a14390e890048726504046783eb2ebed7
Douglas <87678082+douglaspfeffer@users.noreply.github.com>
2021-08-17 17:56:21 -0300

    Subindo Exercícios

--- ex093.py
+++ ex093.py
@@ -3,3 +3,30 @@
 # em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de
 # gols feitos durante o campeonato.
 
+listaDicionario={}
+listaListaGols=[]
+totalGols=0
+
+listaDicionario['Nome']=str(input('Nome do jogador: ')).strip().capitalize()
+totalPartidas=int(input(f'Quantas partidas {listaDicionario["Nome"]} jogou? '))
+
+for c in range(0,totalPartidas):
+    gol=int(input(f'Quantos gols na partida {c+1}? '))
+    listaListaGols.append(gol)
+    totalGols=totalGols+gol
+
+listaDicionario['Gols']=listaListaGols[:]
+listaDicionario['Total']=totalGols
+
+print(f'\n{listaDicionario}\n')
+
+for k,v in listaDicionario.items():
+    print(f'O campo {k} tem o valor {v}')
+
+print()
+
+print(f'O jogador {listaDicionario["Nome"]} jogou {totalPartidas} partidas')
+for p,g in enumerate(listaDicionario['Gols']):
+    print(f'Na partida {p}, fez {g} gols')
+
+print(f'Foi um total de {listaDicionario["Total"]} gols\n')
---
+++ ex094-fazendo.py
@@ -0,0 +1,62 @@
+# Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa
+# em um dicionário e todos os dicionários em uma lista. No final, mostre:
+# A) Quantas pessoas foram cadastradas
+# B) A média de idade
+# C) Uma lista com as mulheres
+# D) Uma lista de pessoas com idade acima da média
+
+from operator import itemgetter
+
+listaDicionario={}
+listaMulheres=[]
+totalPessoasCadastradas=0
+
+somaIdade=0
+mediaIdade=0
+quantidadeMulheres=0
+escolha=''
+
+while True:
+
+    listaDicionario['Nome']=str(input('Nome: ')).strip().capitalize()
+    listaDicionario['Sexo']=str(input('Sexo: [M/F] '))
+
+    if listaDicionario['Sexo']=='f' or listaDicionario['Sexo']=='F':
+        listaMulheres.append(listaDicionario['Nome'])
+        quantidadeMulheres=quantidadeMulheres+1
+
+    while listaDicionario['Sexo']!='m' and listaDicionario['Sexo']!='M' and listaDicionario['Sexo']!='f' and listaDicionario['Sexo']!='F':
+        print('Opção inválida!',end=' ')
+        listaDicionario['Sexo']=str(input('Sexo: [M/F] '))
+        listaMulheres.append(listaDicionario['Nome'])
+        quantidadeMulheres=quantidadeMulheres+1
+
+    listaDicionario['Idade']=int(input('Idade: '))
+    somaIdade=somaIdade+listaDicionario['Idade']
+    totalPessoasCadastradas=totalPessoasCadastradas+1
+    mediaIdade=somaIdade/totalPessoasCadastradas
+
+    escolha=str(input('Deseja continuar? [S/N] '))
+
+    while escolha!='s' and escolha!='S' and escolha!='n' and escolha!='N':
+        print('Opção inválida!',end=' ')
+        escolha=str(input('Deseja continuar? [S/N] '))
+    
+    if escolha=='n' or escolha=='N':
+        break
+
+if totalPessoasCadastradas==1:
+    print(f'\n{totalPessoasCadastradas} pessoa foi cadastrada')
+else:
+    print(f'\n{totalPessoasCadastradas} pessoas foram cadastradas')
+
+print(f'A média de idade é de {mediaIdade:.2f} anos')
+
+if quantidadeMulheres==1:
+    print(f'A mulhere cadastrada foi: {listaMulheres}')
+else:
+    print(f'As mulheres cadastradas foram: {listaMulheres}')
+
+print('Lista de pessoas acima da média:')
+
+    #break
